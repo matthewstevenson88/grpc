5e6d1f9995883b7f5afecbdff1bb274303a881ab
matthewstevenson88 <52979934+matthewstevenson88@users.noreply.github.com>
2020-01-21 10:31:55 -0800

    Fixes from 2 recent merges (#36)
    * Fix conflicts arising from checking number of peer properties.
    
    * Add TODO's.

--- src/core/tsi/s2a/handshaker/s2a_tsi_handshaker.cc
+++ src/core/tsi/s2a/handshaker/s2a_tsi_handshaker.cc
@@ -307,7 +307,8 @@ static tsi_result s2a_handshaker_result_extract_peer(
   s2a_tsi_handshaker_result* result =
       reinterpret_cast<s2a_tsi_handshaker_result*>(
           const_cast<tsi_handshaker_result*>(self));
-  GPR_ASSERT(kTsiS2ANumOfPeerProperties == 2);
+  // TODO(mattstev): add check once security level peer property is added.
+  //GPR_ASSERT(kTsiS2ANumOfPeerProperties == 2);
   tsi_result ok = tsi_construct_peer(kTsiS2ANumOfPeerProperties, peer);
   int index = 0;
   if (ok != TSI_OK) {
@@ -337,7 +338,8 @@ static tsi_result s2a_handshaker_result_extract_peer(
     tsi_peer_destruct(peer);
     gpr_log(GPR_ERROR, "Failed to set TSI peer property.");
   }
-  GPR_ASSERT(++index == kTsiS2ANumOfPeerProperties);
+  // TODO(mattstev): add check once security level peer property is added.
+  //GPR_ASSERT(++index == kTsiS2ANumOfPeerProperties);
   return ok;
 }
 
