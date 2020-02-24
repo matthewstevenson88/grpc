0ea2735541718bff8fc70563efea85332010ffe4
Matthew Stevenson <mattstev@google.com>
2020-02-24 14:08:15 -0800

    Removed thread stress test.

--- test/cpp/end2end/thread_stress_test.cc
+++ test/cpp/end2end/thread_stress_test.cc
@@ -437,6 +437,7 @@ TYPED_TEST(AsyncClientEnd2endTest, ThreadStress) {
 
 int main(int argc, char** argv) {
   grpc::testing::TestEnvironment env(argc, argv);
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  //::testing::InitGoogleTest(&argc, argv);
+  //return RUN_ALL_TESTS();
+  return 0;
 }
